149992babcf0162a42fc2fff06759082e1176010
Sekou KOITA <sictoz@gmail.com>
2013-08-01 18:15:23 +0200

    defining the architecture of the bundle

---
+++ TmsMediaBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * 
+ * @author:  TESSI Marketing <contact@tessi.fr>
+ *
+ */
+
+namespace Tms\Bundle\MediaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TmsMediaBundle extends Bundle
+{
+}
