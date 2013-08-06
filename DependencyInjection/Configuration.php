70cf77e82ee8bde75f18d88d8bda9386b5a2e5f6
Sekou KOITA <sictoz@gmail.com>
2013-08-06 11:12:53 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -26,7 +26,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('idci_notification_api_client');
+        $rootNode = $treeBuilder->root('tms_media');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
