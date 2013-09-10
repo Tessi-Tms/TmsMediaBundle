942ec94bba291ba9599ce9fd816f22cae34e6945
Sekou KOITA <sictoz@gmail.com>
2013-09-10 16:26:37 +0200

    Add Tests directory and subdirectories and add phpunit.xml

---
+++ Tests/Service/ManagerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @author:  Sekou KOÏTA <sekou.koita@supinfo.com>
+ * @license: GPL
+ *
+ */
+
+namespace Tms\Bundle\MediaBundle\Tests\Service;
+
+class ManagerTest extends \PHPUnit_Framework_TestCase
+{
+}
+
