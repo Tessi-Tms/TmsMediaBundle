bda84f308ce6a2a5c005f180f17f61d76ed4455c
Sekou KOITA <sictoz@gmail.com>
2013-09-11 15:02:52 +0200

    Update ManagerTest

---
+++ Tests/Controller/ManagerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @author:  Sekou KOÏTA <sekou.koita@supinfo.com>
+ * @license: GPL
+ *
+ */
+
+namespace Tms\Bundle\MediaBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ManagerTest extends WebTestCase
+{
+    /**
+     * Test add media
+     */
+    public function testAddMedia()
+    {
+    }
+
+    /**
+     * Test retrieve media
+     */
+    public function testRetrieveMedia()
+    {
+    }
+
+    /**
+     * Test delete media
+     */
+    public function testDeleteMedia()
+    {
+    }
+
+    /**
+     * Test guess storage mapper
+     */
+    public function testGuessStorageMapper()
+    {
+    }
+
+}
+
