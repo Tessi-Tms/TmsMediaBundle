132142e323e3bb80aa5dc6946ff95353cd1f5457
Sekou KOITA <sictoz@gmail.com>
2013-09-11 15:08:17 +0200

    Update ApiControllerTest and Composer.json

--- Tests/Controller/ApiControllerTest.php
+++ Tests/Controller/ApiControllerTest.php
@@ -15,17 +15,72 @@ use Tms\Bundle\MediaBundle\Entity\Media;
 
 class ApiControllerTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Test Post
+     */
     public function testPost()
     {
+        // Create a client like the browser
+        $client = static::createClient;
+
+        // Soumission de formulaire avec upload de fichier
+        $photo = new UploadedFile(
+            '/home/sekou/Images/photo.jpg',
+            'photo.jpg',
+            'image/jpeg',
+            44772)
+        ;
+
+        // or
+/*        $photo = array(
+            '/home/sekou/Images/photo.jpg',
+            'name' => 'photo.jpg',
+            'type' => 'image/jpeg',
+            'size' => 123,
+            'error' => UPLOAD_ERR_OK)
+        ;
+*/
+        $crawler = $client->request(
+            'POST',
+            '/media',
+            array(),
+            array('photo' => $photo))
+        ;
     }
 
-    public function deleteActionAction()
+    /**
+     * Test delete
+     */
+    public function testDelete()
     {
+        $client = static::createClient();
+
+        // Exécute une requête DELETE et passe des entête HTTP
+        $client->request(
+            'DELETE',
+            '/media/reference',
+            array(),
+            array(),
+            array())
+        ;
     }
 
-    public function getAction()
+    /**
+     * Test Get
+     */
+    public function testGet()
     {
-    }
+        $client = static::createClient();
 
+        $client->request(
+            'GET',
+            '/media/reference',
+            array(),
+            array(),
+            array(
+                'CONTENT_TYPE' => 'image/jpeg',
+            ))
+        ;
+    }
 }
 
